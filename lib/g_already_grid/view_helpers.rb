547cabb4c516220e15c211679bb24fee07fd46b4
Jason Harrelson <jason@lookforwardenterprises.com>
2010-01-28 19:06:29 -0600

    Not sure why we are having a conflict .

--- lib/g_already_grid/view_helpers.rb
+++ lib/g_already_grid/view_helpers.rb
@@ -1,13 +1,13 @@
 module GAlreadyGrid
   module ViewHelpers
-    
+
     # GUIlded element.  This is a grid that is utilizing the already grid jQuery plugin.  It is
     # a progressively enhanced table.
-    # 
+    #
     # *parameters*
     # +ar_col+ (required) An ActiveRecord collection.
     # +options+ As follows.
-    # 
+    #
     # *options*
     # +id+ (required) The id of the element.  Must be unique on the page.
     # +cols+ (required) An array of column names to include in the table.  Each position of
@@ -15,13 +15,13 @@ module GAlreadyGrid
     #   database field name as a key and the name to use in the gird as the value.  Each
     #   key and value in the hash can be a symbol or string.
     # +skin+ The name of the skin to use for this Guilded element.  Defaults to default => which is default.css.
-    # +exclude_css+ A boolean indicating if Guilded should create a stylesheet inlcude tag for 
+    # +exclude_css+ A boolean indicating if Guilded should create a stylesheet inlcude tag for
     #   this element.  Defaults to false.
     # +empty_msg+ a message to display when the ar_col is empty or nil.  Defaults to 'No matching records'.
     # +actions+ Action links to include in the action column of grid.  A boolean or Array of symbols.  When 
-    #   boolean, if true, add show, edit and delete actions.  When boolean, if false, hide actions column.  
+    #   boolean, if true, add show, edit and delete actions.  When boolean, if false, hide actions column.
     #   When Array of symbols, add only actions present in array.
-    # +paginate+ A boolean indicating whether to output pagination controls.  Defaults to 
+    # +paginate+ A boolean indicating whether to output pagination controls.  Defaults to
     #   true.  Utilizes will_paginate plugin features.
     # +polymorphic_type+ The type of this polymorphic record.  Should be a string representing the class name.
     # +polymorphic_as+ The as used for the polymorphic type.  Can be a symbol or string.
@@ -37,23 +37,32 @@ module GAlreadyGrid
       options = args.extract_options!
       raise ArgumentError, "'cols' option required" unless options.include?( :cols )
       raise ArgumentError, "'cols' option must be an array of columns" unless options[:cols].is_a? Array
-      
+
       Guilded::Guilder.instance.add( :already_grid, options, [ 'jquery/jquery-already_grid-0.1.min.js' ] )
 
       return "<span class=\"list-empty-msg\">#{options[:empty_msg] || 'No matching records'}</span>" if ar_col.nil? || ar_col.empty?
-      
+
       options[:grid_class] ||= 'already-grid'
       options[:checkbox_class] ||= "chk"
       options[:check_all_class] ||= "checkAll"
       options[:date_format] ||= :default
       options[:del_confirmation] ||= 'Are you sure you would like to delete this record?'
       options[:clickable] = true if options[:clickable].nil?
-      
+
       options[:cols], options[:col_titles] = Guilded::Rails::Helpers.resolve_field_methods_and_titles( options[:cols], ar_col[0] )
       path_helpers = Guilded::Rails::Helpers.resolve_rest_path_helpers( ar_col, options )
+
+      if options[:use_sti_base_index] == true
+        specific_name = ar_col.first.class.name.tableize
+        unless ar_col.first.class.superclass.is_a?( ActiveRecord::Base )
+          base_class = ar_col.first.class.superclass
+          base_name = base_class.name.tableize
+          path_helpers[:index_rest_helper] = path_helpers[:index_rest_helper].gsub( /#{specific_name}/, base_name ) unless base_name.nil?
+        end
+      end
       
       scoped_by = options.delete( :scoped_by )
-      
+
       # Resolve sorting
       if options[:sort]
         if options[:sort].is_a?( Array )
@@ -65,7 +74,7 @@ module GAlreadyGrid
         end
       end
       sort_by = Array.new unless sort_by
-      
+
       if options[:actions].is_a?( TrueClass )
         options[:actions] = [ :show, :edit, :delete ]
       elsif options[:actions].nil?
@@ -87,13 +96,13 @@ module GAlreadyGrid
       end
 
       options[:checkboxes] = true if options[:checkboxes].nil?
-      
+
       total_columns = options[:cols].size
       total_columns = total_columns + 1 if options[:checkboxes]
       total_columns = total_columns + 1 unless options[:actions].empty?
       options[:total_columns] = total_columns
-      
-      vars = { 
+
+      vars = {
         :options => options, :ar_col => ar_col, :do_paginate => do_paginate, :sort_by => sort_by, :path_helpers => path_helpers
       }
 
@@ -102,13 +111,13 @@ module GAlreadyGrid
       full_path = "#{path}/templates/guilded.already_grid.html.erb"
       self.render( :file => full_path, :use_full_path => false, :locals => vars )
     end
-    
+
   private
-  
-    # Creates a link to sort this column of a table.  Utilizes the link_to helper, 
-    # but resolves sort by, direction and adds correct CSS classes for UI elements to 
+
+    # Creates a link to sort this column of a table.  Utilizes the link_to helper,
+    # but resolves sort by, direction and adds correct CSS classes for UI elements to
     # be displayed.
-    # 
+    #
     # *parameters*
     # +name+ The text to use for the link
     # +method+ The actual name of this column's field.
@@ -134,21 +143,21 @@ module GAlreadyGrid
       sort_options.merge!( :filter => params[:filter] ) if params[:filter]
       unsorted_options = { :order => method, :direction => 'ASC' } # always sort ascending on first sort
       unsorted_options.merge!( :filter => params[:filter] ) if params[:filter]
-      
+
       path = path.to_sym unless path.is_a?( Symbol )
       path_args = [] + scoping_args
-      
+
       if is_sorted_link
         # Handle the currently sorted by link
         path_args << sort_options
         path = @controller.send( path, *path_args )
       else
-        # Handle the the other currently unsorted by links (we will always sort in 
+        # Handle the the other currently unsorted by links (we will always sort in
         # an ASC direction the first time)
         path_args << unsorted_options
         path = @controller.send( path, *path_args )
       end
-      
+
       # Try to adapt to get collection rest methods
       if already_grid_options[:adaptable_url]
         path_parts = path.split( '?' )
@@ -158,13 +167,13 @@ module GAlreadyGrid
           path = "#{request.path_info}?#{path_parts[1]}"  # path.gsub( /#path_parts[0]/, request.request_uri )
         end
       end
-      
+
       # Load the params to an args array to send to the link_to helper
       args = []
       args << name << path << options
 
       link_to( *args )
     end
-    
+
   end
 end
